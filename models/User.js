afa2368d6dcf71e74f5bfb6b5db78f8be49c4b4b
Yaroslaw <jaroslawjuk@gmail.com>
2018-06-29 14:54:17 +0300

    Starting build REST api

--- models/User.js
+++ models/User.js
@@ -6,35 +6,35 @@ const bcrypt = require('bcryptjs');
 const passportLocalMongoose = require('passport-local-mongoose');
 
 const UserSchema = new Schema({
-    email: {
-        type: String,
-        unique: true,
-        required: true
-    },
-    hashedPassword: {
-        type: String,
-        required: true
-    },
-    createdAt: {type: Date}
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  hashedPassword: {
+    type: String,
+    required: true
+  },
+  createdAt: { type: Date }
 });
 
 UserSchema.virtual('password')
     .set(function (password) {
-        this.salt = bcrypt.genSaltSync(10);
-        this.hashedPassword = this.encryptPassword(password);
+      this.salt = bcrypt.genSaltSync(10);
+      this.hashedPassword = this.encryptPassword(password);
     });
 
 UserSchema.methods = {
 
-    encryptPassword: function (password) {
-        return bcrypt.hashSync(password, this.salt);
-    },
+  encryptPassword: function (password) {
+    return bcrypt.hashSync(password, this.salt);
+  },
 
-    validPassword: function (password) {
-        return bcrypt.compareSync(password, this.hashedPassword);
-    }
+  validPassword: function (password) {
+    return bcrypt.compareSync(password, this.hashedPassword);
+  }
 };
 
-UserSchema.plugin(passportLocalMongoose, {usernameField: 'email'});
+UserSchema.plugin(passportLocalMongoose, { usernameField: 'email' });
 
 module.exports = mongoose.model('User1', UserSchema);
